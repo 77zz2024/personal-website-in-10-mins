1b3af17298f205c857fcc39ff9b49c69b221f209
77zz2024 <zoe19970130@gmail.com>
2024-09-06 02:03:35 -0500

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,7 @@ import About from "./routes/about";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
-  { rel: "icon", href: "pictures/icon_old.png" },
+  { rel: "icon", href: "pictures/icon.png" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
