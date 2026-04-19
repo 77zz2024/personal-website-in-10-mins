164ae41c58337333a2e4ba122efd9acaa2cdf2f2
77zz2024 <zoe19970130@gmail.com>
2026-04-19 15:40:58 -0700

    Update configs.ts

--- app/configs/configs.ts
+++ app/configs/configs.ts
@@ -64,7 +64,7 @@ const configs: Configs = {
           date: "Nov. 2021 – Jun. 2025",
           content: [
             "Maintained highly scalable and distributed RESTful services (Spring Boot) for an enterprise database (Oracle/DB2) which is the single source of the company-wide business rules/parameters, serving 10k+ QPS",
-            "Leveraged CI/CD (Jenkins) for deployment on AWS; used OCP for pod management and Nexus/SonarQube for code analysis",
+            "Leveraged CI/CD (Jenkins) for deployment on AWS; used Kubernetes for pod management and Nexus/SonarQube for code analysis",
             "Built a Frontend Website (React) for the services and created Datadog dashboards for continuous monitoring",
             "Developed a ServiceNow automation tool to facilitate the data change process requested by users, cutting 80% of manual effort",
             "Led the development and maintenance of three Python (Pandas, Numpy, OR-Tools) automation tools, cutting the finance department’s monthly planning time by 40% and reducing errors by 60%",
