164d2417a392c7906cf0ffd7e0a0fb11060f8fb5
77zz2024 <zoe19970130@gmail.com>
2026-04-19 15:17:41 -0700

    Update footPrints.ts

--- app/configs/footPrints.ts
+++ app/configs/footPrints.ts
@@ -11,15 +11,30 @@ export const footPrintsWithPicture: FootPrintData[] = [
     image: "NewYork.jpg",
     title: "New York City",
   },
-    {
+  {
     position: [38.6270, -90.1994],
     image: "saint_louis.jpg",
     title: "Saint Louis",
   },
-    {
+  {
     position: [41.8781, -87.6298],
     image: "chicago.jpg",
     title: "Chicago",
+  },
+  {
+    position: [37.3382, -121.8863],
+    image: "san_jose.jpg",
+    title: "San Jose",
+  },
+  {
+    position: [47.6062, -122.3321],
+    image: "seattle.jpg",
+    title: "Seattle",
+  },
+  {
+    position: [28.2282, 112.9388],
+    image: "changsha.jpg",
+    title: "Changsha",
   },
 ];
 
